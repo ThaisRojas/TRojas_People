fc867f10d07ab18d9b1299125b043b15bb7e7d5b
Thais Rojas <thais.rojas@udla.edu.ec>
2025-06-29 15:24:08 -0500

    Agregar archivos de proyecto.

---
+++ TRojas_People/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TRojas_People
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TRojas_People/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TRojas_People
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
